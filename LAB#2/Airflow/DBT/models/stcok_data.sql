c4cd2a0b2a968e3cfe43771677b663dcda2b3e75
Aayushi2504 <aayushiparag.shah@sjsu.edu>
2024-11-14 15:16:44 -0800

    -

---
+++ LAB#2/Airflow/DBT/models/stcok_data.sql
@@ -0,0 +1,23 @@
+-- models/stock_data.sql
+
+WITH raw_stock_data AS (
+    SELECT 
+        date,
+        symbol,
+        open,
+        max,
+        min,
+        close,
+        volume
+    FROM {{ source('stock_analysis_project', 'market_data') }} 
+)
+
+SELECT
+    date,
+    symbol,
+    open,
+    max,
+    min,
+    close,
+    volume
+FROM raw_stock_data
